6b9cdd82b68e93b80036f5cd20448ef349f00e53
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-18 11:17:07 -0800

    README updated

--- README.md
+++ README.md
@@ -5,3 +5,34 @@
 - Stacks and Queues in C
 - Stack operations
 - Queue operations
+
+# Monty
+
+> An interpreter for the Monty language
+> ## Built with data structure concepts 
+> Some of the concepts include inserting, poping, pushing, inserting, rotating, and handling mathematical operations
+
+## The Monty program
+Monty 0.98 is a scripting language that is first compiled into Monty byte codes (Just like Python). It relies on a unique stack, with specific instructions to manipulate it. The goal of this project is to create an interpreter for Monty ByteCodes files.
+
+### Monty byte code files
+
+Files containing Monty byte codes usually have the .m extension. Most of the industry uses this standard but it is not required by the specification of the language. There is not more than one instruction per line. There can be any number of spaces before or after the opcode and its argument
+
+## Built With
+
+- C Language
+- Shell
+
+## Getting started
+To run the interpreter locally, follow the procedure below
+
+```
+git clone [this repo]
+cd monty
+./monty <file>
+```
+The file has to be a monty file with opcode instructions
+## Authors
+
+👤 **Abdulqudus Oladega**
--- monty.h
+++ monty.h
@@ -1,3 +1,18 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/*===================================================================*/
+/** Structs **/
+/*===================================================================*/
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -27,3 +42,64 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct args - structure that will have the arguments from
+ * main
+ * @ac: the argument count
+ * @file: file from main
+ * @line_number: number of the current line in the file
+ */
+typedef	struct args
+{
+	int ac;
+	char *file;
+	unsigned int line_number;
+} args_a;
+
+/**
+ * struct infr - extern data to be manipulated from
+ * individual functions
+ * @line: line from the file
+ * @words: parsed tokens from line
+ * @stack: pointer to the stack
+ * @file: file pointer
+ * @flag: flag to determine if its a stack or queue
+ */
+typedef struct infr
+{
+	char *line;
+	char **words;
+	stack_t *stack;
+	FILE *file;
+	int flag: 3;
+} infor_s;
+
+extern infor_s infor;
+
+#define INFOR_INIT {NULL, NULL, NULL, NULL, 0}
+
+/*===================================================================*/
+/** Handlers **/
+/*===================================================================*/
+void push_h(stack_t **stack, unsigned int line_number);
+void pall_h(stack_t **stack, unsigned int line_number);
+void pint_h(stack_t **stack, unsigned int line_number);
+void pop_h(stack_t **stack, unsigned int line_number);
+void swap_h(stack_t **stack, unsigned int line_number);
+void add_h(stack_t **stack, unsigned int line_number);
+
+/*===================================================================*/
+/** Helpers **/
+/*===================================================================*/
+int count_ws(char *str);
+char **tokens(char *str);
+void run_cmd(args_a *args);
+void (*get_func(char **parse))(stack_t **, unsigned int);
+
+/*===================================================================*/
+/** Cleaners **/
+/*===================================================================*/
+void release(char **args);
+void release_all(int all);
+void free_stack(char *stack);
